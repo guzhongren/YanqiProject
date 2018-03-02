987ade0ba54b4317432b289ebcc6bea9bfcc1e05
guzhongren <guzhongren@live.cn>
2018-03-02 10:36:01 +0800

    update

--- src/route/index2.js
+++ src/route/index2.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Route, NavLink, Redirect } from 'react-router-dom';
+import { Route,Switch, NavLink, Redirect } from 'react-router-dom';
 
 import Index21 from '../components/index21';
 import Index22 from '../components/index22';
 const Index2 = ({match})=> (
-        <div>
-            <div>
-                <NavLink to={`${match.url}/index21`} exact>首页2</NavLink>
-                <NavLink to={`${match.url}/index22`}>首页2</NavLink>
-            </div>
-            <Route exact path={`${match.url}`} render={()=>(<Redirect to={`${match.url}/index21`}/>)}/>
-            <Route path={`${match.url}/index21`} component={Index21}></Route>
-            <Route path={`${match.url}/index22`} component= {Index22}></Route>
-        </div>
-    )
+  <div>
+    <div>
+      <NavLink to={`${match.url}/index21`} exact>首页2</NavLink>
+      <NavLink to={`${match.url}/index22`}>首页2</NavLink>
+    </div>
+    <Switch>
+      <Route exact path={`${match.url}`} render={()=>(<Redirect to={`${match.url}/index21`}/>)}/>
+      <Route path={`${match.url}/index21`} component={Index21}></Route>
+      <Route path={`${match.url}/index22`} component= {Index22}></Route>
+    </Switch>
+  </div>
+)
 export default Index2;
